dae228cee9d0960c599e9f55c77b5f4a4f973a85
qqaa1541 <zoi4u3@naver.com>
2024-07-25 17:03:51 +0900

    aa

---
+++ opencv/ch07/blur.cpp
@@ -0,0 +1,33 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+String folder = "/home/pdy/KDTA.ROS2/opencv/data/";
+
+int main()
+{
+    Mat src = imread(folder + "rose.bmp", IMREAD_GRAYSCALE);
+    vector<Mat> dsts;
+    int ksize = 5;
+
+    for (int ksize = 3; ksize <= 19; ksize += 2) {
+        Mat dst;
+        blur(src, dst, Size(ksize, ksize));
+        dsts.push_back(dst);
+    }
+   
+    
+    imshow("src", src);
+
+    int i = 3;
+    //for (int i = 0; i < dsts.size(); i++) 
+    for (auto dst : dsts)
+    {
+        imshow("dst" + to_string(i), dst);
+        i += 2;
+    }
+
+    waitKey(0);
+    return 0;
+}
